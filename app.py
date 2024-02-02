ae5d8a11eef74927cef1b4bb1ebe69cc8842c13a
Othman Salahi <132019758+OthmanSALAHI@users.noreply.github.com>
2024-02-02 21:41:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+HOST = '0.0.0.0'
+if os.getenv("ENV_HOST"):
+    HOST = getenv("ENV_HOST")
+else:
+    print("wrong ENV or not defined")
+    return (EXIT_FAILURE)
 
 @app.route('/')
 def index():
@@ -13,4 +20,4 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host=HOST, port=5000, debug=True)

2b39b95e12f10d36ab311821440a58c8e60587e9
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-04 14:52:21 +0100

    change endpoint "

--- api/app.py
+++ api/app.py
@@ -6,7 +6,7 @@ import json
 app = Flask(__name__)
 
 
-@app.route("/states", methods=["GET"])
+@app.route("/apis/states", methods=["GET"])
 def states():
     with open("file.json", "r") as f:
         data = json.load(f)

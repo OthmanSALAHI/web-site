2294e2003b4cdbfc738aa4310a529f126dc2c027
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-04 14:36:43 +0100

    add the app file

--- api/app.py
+++ api/app.py
@@ -1,1 +1,15 @@
-# set up
+#!/usr/bin/python3
+
+from flask import Flask, abort, make_response
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/states", methods=["GET"])
+def states():
+    with open("file.json", "r") as f:
+        data = json.load(f)
+    if data:    
+        return data
+    print("error")

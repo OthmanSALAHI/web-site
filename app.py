f62c3276fea782a7d0318ea10f11098d4a923549
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-11 15:12:05 +0100

    states

--- app.py
+++ app.py
@@ -25,8 +25,9 @@ def index():
 def about():
     return render_template('about.html')
 
-# states = get("http://0.0.0.0:5000/apis/states").json()
-states = []
+states = get("http://0.0.0.0:5000/apis/states").json()
+
+
 @app.route('/states')
 def states_list(states=states):
     if not states:

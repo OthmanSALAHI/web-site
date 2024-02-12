643f67224d9aa6b03808b24b330e3ebc3d9149dd
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-12 18:59:57 +0100

    Remove unused import statement

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 from os import getenv 
 import json
 from requests import get
-from constants import EXIT_FAILURE
+
 
 app = Flask(__name__)
 HOST = '0.0.0.0'

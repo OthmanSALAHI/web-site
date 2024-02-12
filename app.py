0ffbcd1e1bddcb4a152356e2dece339c2c6ca710
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-12 19:01:33 +0100

    Refactor app.py: Remove unused code and fix states_list() function

--- app.py
+++ app.py
@@ -9,11 +9,7 @@ from requests import get
 app = Flask(__name__)
 HOST = '0.0.0.0'
 PORT = 5000
-def check_env():
-    """function that do simple flask app"""
 
-    HOST = getenv("ENV_HOST",'0.0.0.0')
-    PORT = getenv("ENV_PORT", 5000)
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -22,14 +18,12 @@ def index():
 def about():
     return render_template('about.html')
 
-states = get("http://0.0.0.0:5000/apis/states").json()
-
-
 @app.route('/states')
-def states_list(states=states):
+def states_list():
+    states = get("http://0.0.0.0:5000/apis/states").json()
     if not states:
         states = ["error","not found"]
     return render_template("states.html", states=states)
+
 if __name__ == '__main__':
-    check_env()
     app.run(host=HOST, port=PORT, debug=True)
